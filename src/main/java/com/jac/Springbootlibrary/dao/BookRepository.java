035c0516f20d3f9bc7ee4cd413d5db0a1cb219bd
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-07 11:13:44 -0500

    Search books by title Backend Part

--- src/main/java/com/jac/Springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/jac/Springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,11 @@
 package com.jac.Springbootlibrary.dao;
 
 import com.jac.Springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository <Book, Long>{
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
