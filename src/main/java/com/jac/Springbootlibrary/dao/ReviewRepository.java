3741dbb9709fcf97c1a980dca7e28e3e8a82c455
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-12 17:45:45 -0500

    create find book by user email and book Id func, create reviewRequest class under new created requestModels package

--- src/main/java/com/jac/Springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/jac/Springbootlibrary/dao/ReviewRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    //find by book Id
 
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+    // find by userEmail and Book Id
+    Review findByUserEmailAndBookId(String userEmail, Long bookId);
 }
