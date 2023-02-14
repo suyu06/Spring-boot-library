5221ba546e4aea79e1a30e4ff588c094e1039603
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-14 10:27:44 -0500

    configure the '/api/messages/secure/' in our filterChain method in SecurityConfiguration class

--- src/main/java/com/jac/Springbootlibrary/config/SecurityConfiguration.java
+++ src/main/java/com/jac/Springbootlibrary/config/SecurityConfiguration.java
@@ -24,7 +24,8 @@ public class SecurityConfiguration {
                                 .antMatchers("/api/books/secure/**",
                                         "/api/reviews/secure/**",
                                         "/api/messages/secure/**",
-                                        "/api/admin/secure/**")
+                                        "/api/admin/secure/**"
+                                       )
                                 .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
