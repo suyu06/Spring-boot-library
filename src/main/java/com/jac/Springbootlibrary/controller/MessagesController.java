5d3d148a7caedbbec3a62875df0d73fa37b623ec
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-15 21:11:22 -0500

    create putMessage api in MessagesController

--- src/main/java/com/jac/Springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/jac/Springbootlibrary/controller/MessagesController.java
@@ -2,6 +2,7 @@ package com.jac.Springbootlibrary.controller;
 
 
 import com.jac.Springbootlibrary.entity.Message;
+import com.jac.Springbootlibrary.requestModels.AdminQuestionRequest;
 import com.jac.Springbootlibrary.service.MessagesService;
 import com.jac.Springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,19 @@ public class MessagesController {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         //call postMessage() in messagesService to save the message to our database
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    //update a message with admin's response
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value="Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        //extract user email and user type from token
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        // call putMessage() in messageService class
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
